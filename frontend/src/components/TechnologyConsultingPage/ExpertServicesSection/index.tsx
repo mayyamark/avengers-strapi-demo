931b6d017580b2db3a840449062841c4fc648a4c
Mayya Markova <mayya.markova@gmail.com>
2024-01-29 14:17:27 +0200

    Add references to Strapi's implementation for Dynamic zones

--- frontend/src/components/TechnologyConsultingPage/ExpertServicesSection/index.tsx
+++ frontend/src/components/TechnologyConsultingPage/ExpertServicesSection/index.tsx
@@ -12,6 +12,17 @@ const ExpertServicesSection = async () => {
     }
   } = await getExpertServicesSection();
 
+  /**
+   * `links` is a dynamic zone!
+   * 
+   * Here is how Strapi creators have implemented dynamic zones in the frontend:
+   * 1. Render a `BlockManager` component and pass the dynamic zone data
+   * https://github.com/strapi/foodadvisor/blob/ceb9ace8a3de04c6dffa9fb6ccb91102305342bc/client/pages/%5B%5B...slug%5D%5D.js#L20
+   * 2. Inside `BlockManager` there is a `switch` around the `__component` field. Based on the value, the correct component is rendered
+   * https://github.com/strapi/foodadvisor/blob/ceb9ace8a3de04c6dffa9fb6ccb91102305342bc/client/components/shared/BlockManager/index.js#L60
+   * 
+   * Weird, but makes sense!
+   */
   return (
     <section className={styles['expert-services-section']}>
       <div className={styles['section-container']}>
